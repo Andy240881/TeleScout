2af8ed27eea345ecf1564cee4b2f76ac23701f86
尤偉喨 <19980303andy@gmail.com>
2019-04-18 13:56:18 +0800

    Add code

--- app.py
+++ app.py
@@ -172,7 +172,7 @@ def handle_postback(event):
         #fp=open('auto_image2.txt', 'r')
         #url=fp.readline()
         #fp.close()
-            stdin,stdout,stderr=ssh.exec_command('python3 save.py')
+            stdin,stdout,stderr=ssh.exec_command('python3 save.py',get_pty=True)
             time.sleep(6)
             if os.path.isfile("prods_img2.txt"):
                 os.remove('prods_img2.txt')
