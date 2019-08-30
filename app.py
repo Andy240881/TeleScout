460b45620cdd35de360891b2499cc13564d79ce0
尤偉喨 <19980303andy@gmail.com>
2019-08-30 18:05:30 +0800

    Add code

--- app.py
+++ app.py
@@ -48,9 +48,6 @@ def handle_message(event):
     sftp.put('input.txt', '/home/4105056023/user_cookie/'+user_id+'/input2.txt')
     sftp.close()
     ssh_stdin,ssh_stdout,ssh_stderr=ssh.exec_command('python3 QA.py '+user_id,get_pty=True)
-    print('##########\n')
-    print(ssh_stdin)
-    print('##########\n')
     message = TemplateSendMessage(
     alt_text='Buttons template',
     template=ButtonsTemplate(
