aed6c8402096be7c96344e01e5871c9d29fc0f1b
尤偉喨 <19980303andy@gmail.com>
2019-09-03 16:56:10 +0800

    Add code

--- app.py
+++ app.py
@@ -550,6 +550,6 @@ while 1:
             if __name__ == "__main__":
                 port = int(os.environ.get('PORT', 5000))
                 app.run(host='0.0.0.0', port=port)
-except MyException as e:
-    if e.value=="restart":
-        continue
+    except MyException as e:
+        if e.value=="restart":
+            continue
