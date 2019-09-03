30c9c94d05e4d7f1f580c38e4d6eaf3b02986bf8
尤偉喨 <19980303andy@gmail.com>
2019-09-03 16:58:27 +0800

    Add code

--- app.py
+++ app.py
@@ -19,22 +19,22 @@ line_bot_api = LineBotApi('9z2SNrr86ZejumpU4hSMa5xry0tLD263V38C3twWNq9ZTr4eIkxTj
 # Channel Secret
 handler = WebhookHandler('704ac71eddb12e51957d76b6d6bbf514')
 # 監聽所有來自 /callback 的 Post Request
-@app.route("/callback", methods=['POST'])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-    return 'OK'
 user_id=''
 while 1:
     try:
+        @app.route("/callback", methods=['POST'])
+        def callback():
+            # get X-Line-Signature header value
+            signature = request.headers['X-Line-Signature']
+            # get request body as text
+            body = request.get_data(as_text=True)
+            app.logger.info("Request body: " + body)
+            # handle webhook body
+            try:
+                handler.handle(body, signature)
+            except InvalidSignatureError:
+                abort(400)
+            return 'OK'
         # 處理訊息
         @handler.add(MessageEvent, message=TextMessage)
         def handle_message(event):
